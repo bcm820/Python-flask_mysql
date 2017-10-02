ef63df20e18df0f9b86f3999d02168c460a77581
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-10-01 22:45:45 -0400

    Adopting the Pythonic way

---
+++ 0-Fundamentals/01-strings.py
@@ -0,0 +1,42 @@
+# define a function that says hello to name
+def say_hello(name):
+    # indented lines are part of a function
+    if name:
+        print 'Hello, ' + name
+    else:
+        print 'No name'
+
+# call function like this:
+say_hello('Brian')
+
+# concat using comma or +, like in JS
+first = Brian
+print "My name is", Brian   # includes space
+print "My name is" + Brian  # no space
+
+# use curly brackets to inject variables
+# i.e. string interpolation
+last = "Mendoza"
+print "My name is {} {}".format(first, last)
+
+### Old Depcrecated Method ###
+hw = "hello %s" % 'world'
+print hw    # prints 'hello world'
+
+
+
+### STRING METHODS ###
+# Functions run on a string.
+
+# METHOD                    # OUTPUT
+last.upper()                # 'MENDOZA'
+last.count('doza')          # 1 (counts occurrences)
+last.endswith('doza')       # T/F (end with 'doza'?)
+last.find('doza')           # 3 (char index of first occurrence)
+last.isalnum()              # T/F (alphanumeric? letters and numbers only, no spaces or punc)
+# also last.isalpha(), last.isdigit(), last.islower(), last.isupper()
+string.join(list)           # takes list and returns a concatenated string
+string.split()              # returns list of values where string is split (or splits at every place, turning each word into a list)
+
+# For more:
+# https://docs.python.org/2.6/library/string.html
